d357d05147b1f957c0c6e9301a35f537f7b39ca9
Will <kode15333@gmail.com>
2021-07-29 08:11:09 +0900

    refactoring 6.7

---
+++ src/chapter/06/6.7.js
@@ -0,0 +1,32 @@
+let tpHd = "untitled";
+
+result += `<h1>${tpHd}</h1>`;
+
+tpHd = obj.articleTitle;
+
+result += `<h1>${title()}</h1>`;
+
+setTitle(obj.articleTitle);
+
+function title() {
+    return title;
+}
+
+function setTitle(arg) {
+    tpHd = arg;
+}
+
+let _title = "untitled";
+
+function title() {
+    return _title;
+}
+
+function setTitle(arg) {
+    _title = arg;
+}
+
+const cpyNm = "애크미 구스베리";
+
+const companyName = "애크미 구스베리";
+const cpyNm = companyName;
