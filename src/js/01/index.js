37bd44d9759fba7803b49be6de2d1e05201f3e37
junho <kode15333@gmail.com>
2022-05-03 22:45:15 +0900

    format 함수 추출

--- src/js/01/index.js
+++ src/js/01/index.js
@@ -8,9 +8,6 @@ function statement(invoice, plays) {
     let totalAmount = 0;
     let volumeCredits = 0;
     let result = `청구 내역 (고객명 : ${invoice.customer})\n`;
-    const format = new Intl.NumberFormat('en-US', {
-        style: 'currency', currency: 'USD',
-        minimumFractionDigits: 2}).format;
 
     for(let perf of invoice.performances) {
 
@@ -27,6 +24,12 @@ function statement(invoice, plays) {
     console.log(result);
 
     return result;
+
+    function format(aNumber){
+        return new Intl.NumberFormat('en-US', {
+            style: 'currency', currency: 'USD',
+            minimumFractionDigits: 2}).format(aNumber);
+    }
 
     function volumeCreditFor(aPerformance) {
         let volumeCredits = 0;
