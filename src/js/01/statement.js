4fb76899bdd5da9d9fc4aa9ff3a63a817cb9834b
junho <kode15333@gmail.com>
2022-05-03 23:26:01 +0900

    renderHtml 추가

--- src/js/01/statement.js
+++ src/js/01/statement.js
@@ -2,20 +2,37 @@ import invoicesData from "./data/invoices.json";
 import playsData from "./data/plays.json";
 import createStatementData from "./createStatement";
 
-
-
-
-function statement(invoice, plays) {
-    return renderPlainText(createStatementData(invoice,  plays))
+function statement (invoice, plays) {
+    return renderPlainText(createStatementData(invoice, plays));
 }
 
+function renderHtml (data) {
+    let result = `<h1>청구 내역 (고객명 ${data.customer})</h1>\n`;
 
-function renderPlainText(data, plays) {
+    result += "<table>\n";
+    result += "<tr><td>연극</td><th>좌석 수</th><th>금액</th></tr>";
+    for (let perf of data.performances) {
+        result += `
+        <tr>
+            <td>${perf.play.name}</td>
+            <td>${perf.audience}석</td>
+            <td>${usd(perf.amount)}</td>
+        </tr>
+        `;
+    }
+    result += "</table>\n";
+    result += `<p>총액: <em>${usd(data.totalAmount)}</em></p>\n`;
+    result += `<p>적립 포인트: <em>${data.totalVolumeCredits}</em>점</p>\n`;
+
+    return result;
+
+}
+
+function renderPlainText (data, plays) {
     let result = `청구 내역 (고객명 : ${data.customer})\n`;
 
-
-    for(let perf of data.performances) {
-        result += ` ${perf.play.name} : ${usd(perf.amount)} (${perf.audience}석)\n`
+    for (let perf of data.performances) {
+        result += ` ${perf.play.name} : ${usd(perf.amount)} (${perf.audience}석)\n`;
     }
 
     result += `총액: ${usd(data.totalAmount)}\n`;
@@ -24,22 +41,13 @@ function renderPlainText(data, plays) {
 
     return result;
 
-
-    function usd(aNumber){
-        return new Intl.NumberFormat('en-US', {
-            style: 'currency', currency: 'USD',
-            minimumFractionDigits: 2}).format(aNumber / 100);
-    }
-
-
-
-
-
-
-
 }
 
+function usd (aNumber) {
+    return new Intl.NumberFormat("en-US", {
+        style: "currency", currency: "USD",
+        minimumFractionDigits: 2
+    }).format(aNumber / 100);
+}
 
-
-
-statement(invoicesData[0], playsData)
+statement(invoicesData[0], playsData);
