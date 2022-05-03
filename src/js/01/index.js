2f6efba11a90534ed0f2afa0dcb1058ebc2b28b3
junho <kode15333@gmail.com>
2022-05-03 23:02:58 +0900

    invoice 대신 data에서 처리

--- src/js/01/index.js
+++ src/js/01/index.js
@@ -7,14 +7,15 @@ import playsData from "./data/plays.json";
 function statement(invoice, plays) {
     const statementData = {};
     statementData.customer = invoice.customer;
+    statementData.performances = invoice.performances;
 
-    return renderPlainText(statementData, invoice, plays)
+    return renderPlainText(statementData,  plays)
 
-    function renderPlainText(data, invoice, plays) {
+    function renderPlainText(data, plays) {
         let result = `청구 내역 (고객명 : ${data.customer})\n`;
 
 
-        for(let perf of invoice.performances) {
+        for(let perf of data.performances) {
 
             // 청구 내역을 출력한다.
             result += ` ${playFor(perf).name} : ${usd(amountFor(perf) )} (${perf.audience}석)\n`
@@ -26,26 +27,28 @@ function statement(invoice, plays) {
         console.log(result);
 
         return result;
-    }
 
+        function totalAmount() {
+            let result = 0;
+            for (let perf of data.performances) {
 
-    function totalAmount() {
-        let result = 0;
-        for (let perf of invoice.performances) {
-
-            result += amountFor(perf);
+                result += amountFor(perf);
+            }
+            return result;
         }
-        return result;
-    }
 
-    function totalVolumeCredits () {
-        let result = 0;
-        for (let perf of invoice.performances) {
-            result += volumeCreditFor(perf);
+        function totalVolumeCredits () {
+            let result = 0;
+            for (let perf of data.performances) {
+                result += volumeCreditFor(perf);
 
+            }
+            return result;
         }
-        return result;
+
     }
+
+
 
 
 
