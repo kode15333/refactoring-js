1fac4ba75bf2e25e8d88a80db04f7ade307c5356
junho <kode15333@gmail.com>
2022-05-03 22:41:09 +0900

    내부 함수 이동

--- src/js/01/index.js
+++ src/js/01/index.js
@@ -5,6 +5,30 @@ import playsData from "./data/plays.json";
 
 
 function statement(invoice, plays) {
+    let totalAmount = 0;
+    let volumeCredits = 0;
+    let result = `청구 내역 (고객명 : ${invoice.customer})\n`;
+    const format = new Intl.NumberFormat('en-US', {
+        style: 'currency', currency: 'USD',
+        minimumFractionDigits: 2}).format;
+
+    for(let perf of invoice.performances) {
+
+        // 포인트를 적립한다.
+        volumeCredits += Math.max(perf.audience - 30, 0);
+        // 희극 관객 5명마다 추가 포인트를 제공한다.
+        if("comedy" === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5)
+
+        // 청구 내역을 출력한다.
+        result += ` ${playFor(perf).name} : ${format(amountFor(perf) / 100)} (${perf.audience}석)\n`
+        totalAmount += amountFor(perf);
+    }
+
+    result += `총액: ${format(totalAmount/100)}\n`;
+    result += `적립 포인트: ${volumeCredits}점\n`;
+    console.log(result);
+
+    return result;
 
     function playFor(aPerformance) {
         return plays[aPerformance.playID];
@@ -32,31 +56,6 @@ function statement(invoice, plays) {
 
         return result
     }
-
-    let totalAmount = 0;
-    let volumeCredits = 0;
-    let result = `청구 내역 (고객명 : ${invoice.customer})\n`;
-    const format = new Intl.NumberFormat('en-US', {
-        style: 'currency', currency: 'USD',
-        minimumFractionDigits: 2}).format;
-
-    for(let perf of invoice.performances) {
-
-        // 포인트를 적립한다.
-        volumeCredits += Math.max(perf.audience - 30, 0);
-        // 희극 관객 5명마다 추가 포인트를 제공한다.
-        if("comedy" === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5)
-
-        // 청구 내역을 출력한다.
-        result += ` ${playFor(perf).name} : ${format(amountFor(perf) / 100)} (${perf.audience}석)\n`
-        totalAmount += amountFor(perf);
-    }
-
-    result += `총액: ${format(totalAmount/100)}\n`;
-    result += `적립 포인트: ${volumeCredits}점\n`;
-    console.log(result);
-
-    return result;
 }
 
 
