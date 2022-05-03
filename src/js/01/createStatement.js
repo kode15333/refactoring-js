1260d549fb8e442540b1efb824bf400f45ee0b45
junho <kode15333@gmail.com>
2022-05-03 23:35:47 +0900

    PerformanceCalculator amountFor 대체

--- src/js/01/createStatement.js
+++ src/js/01/createStatement.js
@@ -3,6 +3,29 @@ class PerformanceCalculator{
         this.performancee = aPerformance;
         this.play = aPlay;
     }
+
+    get amount(){
+        let result = 0;
+        switch (this.performancee.play.type) {
+            case 'tragedy' :
+                result = 40000;
+                if(this.performancee.audience > 30) {
+                    result += 1000 * (this.performancee.audience - 30)
+                }
+                break;
+            case 'comedy' :
+                result = 30000;
+                if(this.performancee.audience > 20) {
+                    result += 10000 + 500 * (this.performancee.audience - 20);
+                }
+                result += 300 * this.performancee.audience;
+                break;
+            default:
+                throw new Error(`알 수 없는 장르 : ${this.play.type}`)
+        }
+
+        return result
+    }
 }
 
 
@@ -27,27 +50,9 @@ export default function createStatementData (invoice, plays) {
         return plays[aPerformance.playID];
     }
     function amountFor(aPerformance) {
-        let result = 0;
-        switch (aPerformance.play.type) {
-            case 'tragedy' :
-                result = 40000;
-                if(aPerformance.audience > 30) {
-                    result += 1000 * (aPerformance.audience - 30)
-                }
-                break;
-            case 'comedy' :
-                result = 30000;
-                if(aPerformance.audience > 20) {
-                    result += 10000 + 500 * (aPerformance.audience - 20);
-                }
-                result += 300 * aPerformance.audience;
-                break;
-            default:
-                throw new Error(`알 수 없는 장르 : ${aPerformance.play.type}`)
-        }
-
-        return result
+        return  new PerformanceCalculator(aPerformance, playFor(aPerformance)).amount;
     }
+
     function volumeCreditFor(aPerformance) {
         let volumeCredits = 0;
         volumeCredits += Math.max(aPerformance.audience - 30, 0);
