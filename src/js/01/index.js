180042fe6f1f9cf2e8f9d7324f9bbe1936cee023
junho <kode15333@gmail.com>
2022-05-03 23:16:31 +0900

    totalAmount, totalVolumeCredits 계산으로 이동

--- src/js/01/index.js
+++ src/js/01/index.js
@@ -8,6 +8,8 @@ function statement(invoice, plays) {
     const statementData = {};
     statementData.customer = invoice.customer;
     statementData.performances = invoice.performances.map(entirePerformance);
+    statementData.totalAmount = totalAmount(statementData);
+    statementData.totalVolumeCredits = totalVolumeCredits(statementData)
 
     return renderPlainText(statementData,  plays)
 
@@ -56,26 +58,7 @@ function statement(invoice, plays) {
     }
 
 
-
-}
-function renderPlainText(data, plays) {
-    let result = `청구 내역 (고객명 : ${data.customer})\n`;
-
-
-    for(let perf of data.performances) {
-
-        // 청구 내역을 출력한다.
-        result += ` ${perf.play.name} : ${usd(perf.amount )} (${perf.audience}석)\n`
-    }
-
-
-    result += `총액: ${usd(totalAmount())}\n`;
-    result += `적립 포인트: ${totalVolumeCredits()}점\n`;
-    console.log(result);
-
-    return result;
-
-    function totalAmount() {
+    function totalAmount(data) {
         let result = 0;
         for (let perf of data.performances) {
 
@@ -84,7 +67,8 @@ function renderPlainText(data, plays) {
         return result;
     }
 
-    function totalVolumeCredits () {
+
+    function totalVolumeCredits (data) {
         let result = 0;
         for (let perf of data.performances) {
             result += perf.volumeCredits;
@@ -92,6 +76,25 @@ function renderPlainText(data, plays) {
         }
         return result;
     }
+
+
+
+}
+function renderPlainText(data, plays) {
+    let result = `청구 내역 (고객명 : ${data.customer})\n`;
+
+
+    for(let perf of data.performances) {
+        result += ` ${perf.play.name} : ${usd(perf.amount)} (${perf.audience}석)\n`
+    }
+
+    result += `총액: ${usd(data.totalAmount)}\n`;
+    result += `적립 포인트: ${data.totalVolumeCredits}점\n`;
+    console.log(result);
+
+    return result;
+
+
     function usd(aNumber){
         return new Intl.NumberFormat('en-US', {
             style: 'currency', currency: 'USD',
