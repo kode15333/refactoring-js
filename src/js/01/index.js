4d2508a489d421e84e76be68969ba9918c0feefd
junho <kode15333@gmail.com>
2022-05-03 22:34:38 +0900

    amountFor play -> playFor변경

--- src/js/01/index.js
+++ src/js/01/index.js
@@ -2,33 +2,35 @@ import invoicesData from "./data/invoices.json";
 import playsData from "./data/plays.json";
 
 
-function amountFor(aPerformance, play) {
-    let result = 0;
-    switch (play.type) {
-        case 'tragedy' :
-            result = 40000;
-            if(aPerformance.audience > 30) {
-                result += 1000 * (aPerformance.audience - 30)
-            }
-            break;
-        case 'comedy' :
-            result = 30000;
-            if(aPerformance.audience > 20) {
-                result += 10000 + 500 * (aPerformance.audience - 20);
-            }
-            result += 300 * aPerformance.audience;
-            break;
-        default:
-            throw new Error(`알 수 없는 장르 : ${play.type}`)
-    }
 
-    return result
-}
 
 function statement(invoice, plays) {
 
     function playFor(aPerformance) {
         return plays[aPerformance.playID];
+    }
+
+    function amountFor(aPerformance, play) {
+        let result = 0;
+        switch (playFor(aPerformance).type) {
+            case 'tragedy' :
+                result = 40000;
+                if(aPerformance.audience > 30) {
+                    result += 1000 * (aPerformance.audience - 30)
+                }
+                break;
+            case 'comedy' :
+                result = 30000;
+                if(aPerformance.audience > 20) {
+                    result += 10000 + 500 * (aPerformance.audience - 20);
+                }
+                result += 300 * aPerformance.audience;
+                break;
+            default:
+                throw new Error(`알 수 없는 장르 : ${playFor(aPerformance).type}`)
+        }
+
+        return result
     }
 
     let totalAmount = 0;
