37ab6eac47db9590d75a3cc73670c5884d8a962c
junho <kode15333@gmail.com>
2022-05-06 17:39:35 +0900

    매개변수 추가하기 - 쉬운방법 : ide 사용하기 - 어려울때 : migration처리  - 본문을 새로운 함수로 만들어 호출한다(함수명 바꾸기)  - 기존 함수해서는 default값을 넣어주고, 새로운함수에서 추가한다음 테스트를 진행한다.

--- src/js/06/changeFunctionDeclaration.js
+++ src/js/06/changeFunctionDeclaration.js
@@ -5,3 +5,26 @@ function circum(radius) {
 function circumferance(radius) {
     return 2 * Math.PI * radius;
 }
+
+{
+    class Book {
+        addReservation (customer) {
+            this.zz_addReservation(customer);
+        }
+
+        zz_addReservation (customer) {
+            this._reservations.push(customer);
+        }
+    }
+
+}
+
+class Book {
+    addReservation (customer) {
+        this.zz_addReservation(customer, false);
+    }
+
+    zz_addReservation (customer, isPriority) {
+        this._reservations.push(customer);
+    }
+}
