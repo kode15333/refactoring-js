0381f17c676f047ef862c3095355fe97efd53461
junho <kode15333@gmail.com>
2022-05-06 19:23:02 +0900

    단계 쪼개기
    하나의 함수는 하나의 역할만 해야한다.
    역할이 분리할 수 잇다면 별도의 함수로 분리한다.
    값을 공유하는 형식 (파라미터) 또한 두 함수가 이어질수 잇게 만들어주면
    함수를 호출하기 편한다.

---
+++ src/js/06/splitPharse.js
@@ -0,0 +1,40 @@
+{
+
+    function priceOrder (product, quantity, shippingMethod) {
+        const basePrice = product.basePrice + quantity;
+        const discount = Math.max(quantity - product.discountThreshold, 0)
+            * product.basePrice * product.discountRate;
+        const shippingPerCase = (basePrice > shippingMethod.discountThreshold)
+            ? shippingMethod.discountedFee : shippingMethod.feePerCase;
+        const shippingCost = quantity * shippingPerCase;
+        const price = basePrice - discount + shippingCost;
+        return price;
+    }
+}
+
+{
+
+    function priceOrder (product, quantity, shippingMethod) {
+        const priceData = calculatePricingData(product, quantity);
+        return applyShipping(priceData, shippingMethod);
+    }
+
+    function calculatePricingData (product, quantity) {
+        const basePrice = product.basePrice + quantity;
+        const discount = Math.max(quantity - product.discountThreshold, 0)
+            * product.basePrice * product.discountRate;
+
+        return { basePrice, quantity, discount };
+    }
+
+    function applyShipping (priceData, shippingMethod) {
+        const shippingPerCase = (priceData.basePrice > shippingMethod.discountThreshold)
+            ? shippingMethod.discountedFee : shippingMethod.feePerCase;
+        const shippingCost = priceData.quantity * shippingPerCase;
+        const price = priceData.basePrice - priceData.discount + shippingCost;
+        return price;
+
+    }
+}
+
+
