1ebcbc9696393f269cbb772d797bf912ef4909b0
junho <kode15333@gmail.com>
2022-05-07 23:15:07 +0900

    필드 옮기기 데이터 구조를 제대로 잡아야지 개발하기 쉽다 언제 필드를 옮겨야 할까, 수정해야되는데 이것뿐만 아니라 다른 레코드도 수정하고 있을때,
    도메인에 따라 해당 레코드가 가지고 있어야 할 데이터는 언제나 파악하고 수정될 수 있어야 한다.
    레코드 구조가 잘못되었다면, 수정해야 한다! 중요한다.

---
+++ src/js/08/moveField.js
@@ -0,0 +1,56 @@
+class Customer {
+    constructor (name, discountRate) {
+        this._name = name;
+        this._contract = new CustomerContract(dateToday());
+        this._setDiscountRate(discountRate);
+    }
+
+    get discountRate () {
+        return this._contract.discountRate;
+    }
+
+    _setDiscountRate (aNumber) {
+        this._contract.discountRate = aNumber;
+    }
+
+    becomePreferred () {
+        this._setDiscountRate(this.discountRate += 0.03);
+    }
+
+    applyDiscount (amount) {
+        return amount.subtract(amount.multiply(this.discountRate));
+    }
+}
+
+class CustomerContract {
+    constructor (startDate, discountRate) {
+        this._startDate = startDate;
+        this._discountRate = discountRate;
+    }
+
+    get discountRate () {
+        return this._discountRate;
+    }
+
+    set discountRate (arg) {
+        this._discountRate = arg;
+    }
+}
+
+class Account {
+    constructor (number, type, interestRate) {
+        this._number = number;
+        this._type = type;
+    }
+
+    get interestRate () {return this._type.interestRate;}
+}
+
+class AccountType {
+    constructor (nameString, interestRate) {
+        this._name = nameString;
+        this._interestRate = interestRate;
+    }
+
+    get interestRate () {return this._interestRate;}
+}
