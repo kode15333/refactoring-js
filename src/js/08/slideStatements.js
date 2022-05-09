8bf809728e95f3feea9865c9c7a5692e8ecf2514
junho <kode15333@gmail.com>
2022-05-09 09:53:18 +0900

    조건부에서 문장 슬라이드하기
    슬라이드 단위는 문장이 될수도 있고 한줄이 될수도 잇다.
    이러한 부분은 개발자로  위임 하지만,
    작게해야지 결국에 크게 할 수 도 있다.

--- src/js/08/slideStatements.js
+++ src/js/08/slideStatements.js
@@ -13,3 +13,15 @@ discount = discountableUnits * pricingPlan.discountFactor;
 if(order.isRepeat) discount += 20;
 charge = charge - discount;
 chargeOrder(charge)
+
+
+let result;
+if(availableResources.length === 0) {
+    result = createResource();
+    allocatedResources.push(result);
+} else {
+    result = availableResources.pop();
+    allocatedResources.push(result);
+}
+
+return result;
