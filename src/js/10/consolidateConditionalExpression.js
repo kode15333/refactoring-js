176f92b3c3aa92e270cc7af98936efa645d052f2
junho <kode15333@gmail.com>
2022-05-10 00:28:08 +0900

    조건식 통합하기 and 조건 활용하여 보기 쉽게 만들고 함수로 만들어주면 좋다

--- src/js/10/consolidateConditionalExpression.js
+++ src/js/10/consolidateConditionalExpression.js
@@ -9,8 +9,32 @@
 function disabilityAmount (anEmployee) {
     if (isNotEligibleForDisablility()) return 0;
 
-    function isNotEligibleForDisablility(){
-        return anEmployee.seniority < 2 || anEmployee.monthsDisabled > 12 || anEmployee.isPartTime
+    function isNotEligibleForDisablility () {
+        return anEmployee.seniority < 2 || anEmployee.monthsDisabled > 12 || anEmployee.isPartTime;
     }
 }
 
+{
+    const foo = () => {
+        if (anEmployee.onVacation) {
+            if (anEmployee.seniority > 10) {
+                return 1;
+            }
+        }
+
+        return 0.5;
+    };
+
+}
+
+const foo = () => {
+    if ((anEmployee.onVacation) && (anEmployee.seniority > 10)) {
+        return 1;
+    }
+
+    return 0.5;
+
+    function isSeniorVacation () {
+        return ((anEmployee.onVacation) && (anEmployee.seniority > 10));
+    }
+};
