a2f17e945ac1695111001284e3d43f685bf5b703
junho <kode15333@gmail.com>
2022-05-11 07:08:07 +0900

    객체 통채통 넘기기 객체에서 값을 빼내어 인자로 넘겨주는 작업 여러 함수에서 살펴보면 분명 중복되는 작업일 경우가 크다. 하나의 객체로 넘겨줘서 하나의 함수로 만들어준다.

---
+++ src/js/11/preserveWholeObject.js
@@ -0,0 +1,30 @@
+{
+    const low = aRoom.daysTempRange.low;
+    const high = aRoom.daysTempRange.high;
+    if (!aPlan.withinRange(low, high)) {
+        alert.push('방 온도가 지정 범위를 벗어났습니다.')
+    }
+
+    class HeatingPlan {
+        withinRange(bottom, top) {
+            return (bottom >= this._temperatureRange.low && top <= this._temperatureRange.high)
+        }
+
+
+        xxNewWithinRange(aNumberRange) {
+            return this.withinRange(aNumberRange.low, aNumberRange.high);
+        }
+    }
+}
+class HeatingPlan {
+
+    withinRange(aNumberRange) {
+        return (aNumberRange.low >= this._temperatureRange.low && aNumberRange.high <= this._temperatureRange.high)
+    }
+}
+
+{
+    if (!aPlan.withinRange(aRoom.daysTempRange)) {
+        alert.push('방 온도가 지정 범위를 벗어났습니다.')
+    }
+}
