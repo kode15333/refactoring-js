ca91a155092044d015659ac093a9a6b7d29e994a
junho <kode15333@gmail.com>
2022-05-11 00:16:54 +0900

    질의함수와 변경함수 분리하기 한 함수에 조회나 부수효과하는 부분이 있다면 무조건 나누어라(일단)
    좋은 함수는 데이터를 갱신하는 함수와 그저 조회만 하는 함수를 명확히 구분해야한다.
    
    기존 함수에서 복사를 한다음
    기존 함수를 조회함수 새로운 함수를 부수효과 함수를 변경한다.

---
+++ src/js/11/seperateQueryFromModifier.js
@@ -0,0 +1,50 @@
+{
+    function alertForMiscreant (people) {
+        for (const p of people) {
+            if (p === "조커") {
+                setOffAlarms();
+                return "조커";
+            }
+            if (p === "사루만") {
+                setOffAlarms();
+                return "사루만";
+            }
+        }
+        return "";
+    }
+
+    const found = alertForMiscreant(people);
+}
+
+function alertForMiscreant (people) {
+    for (const p of people) {
+        if (p === "조커") {
+            setOffAlarms();
+            return "";
+        }
+        if (p === "사루만") {
+            setOffAlarms();
+            return "";
+        }
+    }
+    return "";
+}
+
+function findMiscreant (people) {
+    if(findMiscreant(people) !== "") setOffAlarms();
+    for (const p of people) {
+        if (p === "조커") {
+            return "조커";
+
+        }
+        if (p === "사루만") {
+            return "사루만";
+
+        }
+    }
+    return "";
+}
+
+const found = findMiscreant(people);
+alertForMiscreant(people);
+
