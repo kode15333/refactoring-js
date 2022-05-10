8da4fed3d3740ad263429278b3c4c083bf1d35ee
junho <kode15333@gmail.com>
2022-05-11 07:14:49 +0900

    객체 통채통 넘기기
    작은 단위로 리펙토링 진행 그럼 함수가 분리가 됨
    
    변수를 객체로 하나로 넘김
    
    새로운 함수 구현

--- src/js/11/preserveWholeObject.js
+++ src/js/11/preserveWholeObject.js
@@ -2,29 +2,49 @@
     const low = aRoom.daysTempRange.low;
     const high = aRoom.daysTempRange.high;
     if (!aPlan.withinRange(low, high)) {
-        alert.push('방 온도가 지정 범위를 벗어났습니다.')
+        alert.push("방 온도가 지정 범위를 벗어났습니다.");
     }
 
     class HeatingPlan {
-        withinRange(bottom, top) {
-            return (bottom >= this._temperatureRange.low && top <= this._temperatureRange.high)
+        withinRange (bottom, top) {
+            return (bottom >= this._temperatureRange.low && top <= this._temperatureRange.high);
         }
 
-
-        xxNewWithinRange(aNumberRange) {
+        xxNewWithinRange (aNumberRange) {
             return this.withinRange(aNumberRange.low, aNumberRange.high);
         }
     }
 }
+
 class HeatingPlan {
 
-    withinRange(aNumberRange) {
-        return (aNumberRange.low >= this._temperatureRange.low && aNumberRange.high <= this._temperatureRange.high)
+    withinRange (aNumberRange) {
+        return (aNumberRange.low >= this._temperatureRange.low && aNumberRange.high <= this._temperatureRange.high);
     }
 }
 
 {
     if (!aPlan.withinRange(aRoom.daysTempRange)) {
-        alert.push('방 온도가 지정 범위를 벗어났습니다.')
+        alert.push("방 온도가 지정 범위를 벗어났습니다.");
     }
+}
+{
+
+    class HeatingPlan {
+        xxNEWwithinRange (aPlan, tempRange) {
+            const low = tempRange.low;
+            const high = tempRange.high;
+            const isWithinRange = aPlan.withinRange(low, high);
+            return isWithinRange;
+        }
+    }
+
+    const tempRange = aRoom.daysTempRange;
+    const isWithinRange = aPlan.xxNEWwithinRange(tempRange);
+    if (!isWithinRange) {
+        alert.push("방 온도가 지정 범위를 벗어났습니다.");
+    }
+
+
+
 }
