90b3baf676089ab39886cecae01a990fc2889ec5
junho <kode15333@gmail.com>
2022-05-12 23:49:41 +0900

    명령을 함수로 바꾸기
    무조건 정답은 없다.
    명령은 복잡한 계산이나, 여러개의 작은 메서드로 쪼개고 필드를 이용해 메서들끼리 정보를 공유할 수 있다. 또한 어떤 메서드를 호출하는지에 따라 다른 효과를 줄 수 있다. 메서드를 호출해가면서 하나의 레코드를 만드는 것
    
    하지만, 간단하다면, 굳이 메서드끼리 상태가 공유되지 않고 하나의 결과값이 나온다면, 함수로 바꿔준다

---
+++ src/js/11/replaceCommandWithFunction.js
@@ -0,0 +1,24 @@
+{
+    class ChargeCalculator{
+        constructor(customer, usage, provider) {
+            this._customer = customer;
+            this._usage = usage;
+            this._provider = provider;
+        }
+
+        get baseCharge(){
+            return this._customer.baseRate * this._usage;
+        }
+
+        get charge(){
+            return this.baseCharge + this._provider.connectionCharge
+        }
+    }
+
+    monthCharge = new ChargeCalculator(customer, usage, provider).charge;
+}
+
+function charge(customer, usage, provider) {
+    const baseCharge = customer.bbaseRate * usage;
+    return baseCharge * provider.connectionCharge
+}
