65ac4ba87ab040efd1a4bc321c668c3ac3672659
junho <kode15333@gmail.com>
2022-05-14 01:48:20 +0900

    서브 클래스르 위임으로 바꾸기
    상속은 무조건 안좋은거다?
    아니다 상속과 위임을 같이 써라
    
    먼저 상속을 하고 상속에 대한 문제점이 생기면 위임으로 처리한다.
    슈퍼클래스를 수정하는데 서브클래스까지 영향이 끼칠대
    위임을 하면서도 위임에 대해서 상속이 구현될수도 있다.

--- src/js/12/replaceSubclassWithDelegate.js
+++ src/js/12/replaceSubclassWithDelegate.js
@@ -9,8 +9,8 @@
             return (this._premiumDelegate) ? this._premiumDelegate.hashTalkBack : this._show.hasOwnProperty("talkBack") && !this.isPeakDay;
         }
 
-        get hasDinner(){
-            return (this._premiumDelegate) ?  this._premiumDelegate.hasDinner : undefined
+        get hasDinner () {
+            return (this._premiumDelegate) ? this._premiumDelegate.hasDinner : undefined;
         }
 
         get basePrice () {
@@ -23,8 +23,6 @@
             this._premiumDelegate = new PremiumBookingDelegate(this, extras);
         }
     }
-
-    
 
     function createBooking (show, date) {
         return new Booking(show, date);
@@ -60,4 +58,90 @@
 
 }
 
+function createBird (data) {
+    return new Bird(data)
+}
 
+class Bird {
+    constructor (data) {
+        this._name = data.name;
+        this._plumage = data.plumage;
+        this._speciesDelegate = this.selectSpeciesDelegate(data);
+    }
+
+    get name () {
+        return this._name;
+    }
+
+    get plumage () {
+        return this._speciesDelegate.plumage;
+    }
+
+    get airSpeedVelocity () {
+        return this._speciesDelegate.airSpeedVelocity
+    }
+
+    selectSpeciesDelegate (data) {
+        switch (data.type) {
+            case "유럽 제비" :
+                return new EuropeanShallowDelegate(data, this);
+            case "아프리카 제비" :
+                return new AfricanSwallowDelegate(data, this);
+            case "노르웨이 파랑 앵무" :
+                return new NorwegianBlueParrotDelegate(data, this);
+            default :
+                return new SpeciesDelegate(data, this);
+        }
+    }
+}
+
+class SpeciesDelegate {
+    constructor (data, bird) {
+        this._bird = bird;
+    }
+
+    get plumage () {
+        return this._bird._plumage || "보통이다";
+    }
+
+    get airSpeedVelocity () {
+        return null;
+    }
+
+}
+
+class EuropeanShallowDelegate extends SpeciesDelegate {
+
+    get airSpeedVelocity () {
+        return 35;
+    }
+}
+
+class AfricanSwallowDelegate extends SpeciesDelegate {
+    constructor (data, bird) {
+        super(data, bird);
+        this._numberOfCoconuts = data.numberOfCoconuts;
+    }
+
+    get airSpeedVelocity () {
+        return 40 - 2 * this._numberOfCoconuts;
+    }
+}
+
+class NorwegianBlueParrotDelegate extends SpeciesDelegate {
+    constructor (data, bird) {
+        super(data, bird);
+        this._voltage = data.voltage;
+        this._isNailed = data.isNailed;
+    }
+
+    get plumage () {
+        if (this._voltage > 100) return "그을렸다";
+        else return this._bird._plumage || "예쁘다";
+    }
+
+    get airSpeedVelocity () {
+        return (this._isNailed) ? 0 : 10 + this._voltage / 10;
+    }
+
+}
