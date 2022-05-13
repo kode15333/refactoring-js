9c7ab6c36f460f4c9a4a273bffe84dd4a6bc8dc2
junho <kode15333@gmail.com>
2022-05-14 00:26:22 +0900

    필드 내리기
    특정 서브클래서에만 필요한 필드는 해당 서브클래스로 내린다.
    
    만약 다른 서브클래스도 필요하다면 다형성을 이용해 구현하자

---
+++ src/js/12/pushDownField.js
@@ -0,0 +1,20 @@
+{
+    class Employee {
+        static quota;
+    }
+
+    class Engineer extends Employee {}
+
+    class Salesman extends Employee {}
+}
+
+
+class Employee {
+}
+
+class Engineer extends Employee {}
+
+class Salesman extends Employee {
+    static quota;
+}
+
