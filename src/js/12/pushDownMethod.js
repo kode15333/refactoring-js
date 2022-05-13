6585c5a6b1670dcc6d629cdc6b79e7921605f128
junho <kode15333@gmail.com>
2022-05-14 00:25:31 +0900

    메서드 내리기
    특정 서브클래서에만 필요한 메서드는 해당 서브클래스로 내린다.
    
    만약 다른 서브클래스도 필요하다면 다형성을 이용해 구현하자

---
+++ src/js/12/pushDownMethod.js
@@ -0,0 +1,20 @@
+{
+    class Employee {
+        get quota () {}
+    }
+
+    class Engineer extends Employee {}
+
+    class Salesman extends Employee {}
+}
+
+
+class Employee {
+}
+
+class Engineer extends Employee {}
+
+class Salesman extends Employee {
+    get quota () {}
+}
+
