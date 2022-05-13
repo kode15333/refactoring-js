03d4d67718287c75080856d651349bce62d359b3
junho <kode15333@gmail.com>
2022-05-14 00:15:37 +0900

    필드 올리기 부모에서 하나만 가지고 있으면 된다. 필드 중복 제거

---
+++ src/js/12/pullUpField.js
@@ -0,0 +1,17 @@
+class Employee{}
+
+class SalesmanA extends Employee {
+    static name;
+}
+
+class SalesmanB extends Employee {
+    static name;
+}
+
+
+{
+    class Employee{
+        static name;
+    }
+
+}
